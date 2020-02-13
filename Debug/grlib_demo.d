5d9eb7bb5de867cfbd84d9b003fc98362dbca5e4
mark_hinkle <mark.hinkle@lasp.colorado.edu>
2020-02-13 10:24:26 -0700

    On branch master Initial commit 	new file:   .ccsproject 	new file:   .cproject 	new file:   .launches/grlib_demo.launch 	new file:   .project 	new file:   .settings/org.eclipse.cdt.codan.core.prefs 	new file:   .settings/org.eclipse.cdt.debug.core.prefs 	new file:   .settings/org.eclipse.core.resources.prefs 	new file:   grlib_demo.c 	new file:   grlib_demo_ccs.cmd 	new file:   images.c 	new file:   images.h 	new file:   macros.ini_initial 	new file:   startup_ccs.c 	new file:   target_config.ccxml

---
+++ Debug/grlib_demo.d
@@ -0,0 +1,83 @@
+# FIXED
+
+grlib_demo.obj: ../grlib_demo.c
+grlib_demo.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h
+grlib_demo.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h
+grlib_demo.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h
+grlib_demo.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h
+grlib_demo.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h
+grlib_demo.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h
+grlib_demo.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h
+grlib_demo.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h
+grlib_demo.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_memmap.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_nvic.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_sysctl.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/fpu.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/flash.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/sysctl.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/systick.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/uart.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/udma.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/driverlib/rom.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/grlib/grlib.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/grlib/widget.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/grlib/canvas.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/grlib/checkbox.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/grlib/container.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/grlib/pushbutton.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/grlib/radiobutton.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/grlib/slider.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/utils/ustdlib.h
+grlib_demo.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdarg.h
+grlib_demo.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/time.h
+grlib_demo.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_ti_config.h
+grlib_demo.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/linkage.h
+grlib_demo.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/abi_prefix.h
+grlib_demo.obj: C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/xlocale/_time.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/examples/boards/ek-tm4c123gxl-boostxl-kentec-s1/drivers/Kentec320x240x16_ssd2119_spi.h
+grlib_demo.obj: C:/ti/tivaware_c_series_2_1_4_178/examples/boards/ek-tm4c123gxl-boostxl-kentec-s1/drivers/touch.h
+grlib_demo.obj: ../images.h
+
+../grlib_demo.c: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_stdint40.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/cdefs.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_types.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/machine/_stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/sys/_stdint.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdbool.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_memmap.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_nvic.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_sysctl.h: 
+C:/ti/tivaware_c_series_2_1_4_178/inc/hw_types.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/fpu.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/gpio.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/flash.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/sysctl.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/systick.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/uart.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/udma.h: 
+C:/ti/tivaware_c_series_2_1_4_178/driverlib/rom.h: 
+C:/ti/tivaware_c_series_2_1_4_178/grlib/grlib.h: 
+C:/ti/tivaware_c_series_2_1_4_178/grlib/widget.h: 
+C:/ti/tivaware_c_series_2_1_4_178/grlib/canvas.h: 
+C:/ti/tivaware_c_series_2_1_4_178/grlib/checkbox.h: 
+C:/ti/tivaware_c_series_2_1_4_178/grlib/container.h: 
+C:/ti/tivaware_c_series_2_1_4_178/grlib/pushbutton.h: 
+C:/ti/tivaware_c_series_2_1_4_178/grlib/radiobutton.h: 
+C:/ti/tivaware_c_series_2_1_4_178/grlib/slider.h: 
+C:/ti/tivaware_c_series_2_1_4_178/utils/ustdlib.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/stdarg.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/time.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/_ti_config.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/linkage.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/abi_prefix.h: 
+C:/ti/ccs920/ccs/tools/compiler/ti-cgt-arm_18.12.3.LTS/include/xlocale/_time.h: 
+C:/ti/tivaware_c_series_2_1_4_178/examples/boards/ek-tm4c123gxl-boostxl-kentec-s1/drivers/Kentec320x240x16_ssd2119_spi.h: 
+C:/ti/tivaware_c_series_2_1_4_178/examples/boards/ek-tm4c123gxl-boostxl-kentec-s1/drivers/touch.h: 
+../images.h: 
---
+++ Debug/subdir_vars.mk
@@ -0,0 +1,41 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+SHELL = cmd.exe
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../grlib_demo_ccs.cmd 
+
+C_SRCS += \
+../grlib_demo.c \
+../images.c \
+../startup_ccs.c 
+
+C_DEPS += \
+./grlib_demo.d \
+./images.d \
+./startup_ccs.d 
+
+OBJS += \
+./grlib_demo.obj \
+./images.obj \
+./startup_ccs.obj 
+
+OBJS__QUOTED += \
+"grlib_demo.obj" \
+"images.obj" \
+"startup_ccs.obj" 
+
+C_DEPS__QUOTED += \
+"grlib_demo.d" \
+"images.d" \
+"startup_ccs.d" 
+
+C_SRCS__QUOTED += \
+"../grlib_demo.c" \
+"../images.c" \
+"../startup_ccs.c" 
+
+
