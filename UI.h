4e69cd8ccc4755a99540cb633c0eb9dc25d0d444
mark_hinkle <mark.hinkle@lasp.colorado.edu>
2020-02-14 16:13:46 -0700

    Create new implementation files for integration

---
+++ UI.h
@@ -0,0 +1,27 @@
+/*
+ * UI.h
+ *
+ *  Created on: Feb 14, 2020
+ *      Author: marka
+ */
+
+#ifndef UI_H_
+#define UI_H_
+
+// *** Includes
+//
+#include <stdio.h>
+#include <stdint.h>
+//
+#include "pushbutton.h"
+#include "images.h"
+
+// *** Defines
+//
+#define UI_UTMENU_SEL_MEM_X                 60
+#define UI_UTMENU_SEL_MEM_Y                 60
+#define UI_UTMENU_SEL_MEM_W                 200
+#define UI_UTMENU_SEL_MEM_H                 50
+
+
+#endif /* UI_H_ */
